c4da577f5029ee8b57fb809019fa2099794f8952
desivar <jillian_desire@yahoo.es>
2025-07-08 04:13:52 -0600

    Header

---
+++ drawing-converter/src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Professional 2D Drawing to 3D Converter</h1>
+            <p>Transform your 2D drawings into stunning 3D models with advanced edge detection and extrusion</p>
+        </div>
+    );
+};
+
+export default Header;
